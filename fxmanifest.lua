a551bfa7379ec952c9b678936aea4ccabef8f3b5
Schrottsoke660 <thomsenlasse@outlook.de>
2026-09-25 23:03:11 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+shared_script '@r820qs/ai_module_fg-obfuscated.lua'
+fx_version 'cerulean'
+game 'gta5'
+
+autohr 'Schrottsoke'
+description 'Real Life California State'
+version '1.1.0'
+
+-- Lua 5.4
+lua54 'yes'
+
+client_script 'client.lua'
